1060458bb7bfa53b6f40b57f814bf63551afddec
Akshay Yelle <02b3akshay@gmail.com>
2025-07-27 18:12:06 +0530

    add react components

--- App.js
+++ App.js
@@ -7,11 +7,15 @@ const Title = () => {
   return <h1 className="head">Namaste React from JSX 🚀</h1>
 }
 
+const number = 1000
+
 // React Functional Component
 const Heading = () => {
   return (
     <div id="container">
       <Title />
+      <h2>{console.log(number)}</h2>
+      {Title()}
       <h1>Namaste React Functional Component</h1>
     </div>
   )
