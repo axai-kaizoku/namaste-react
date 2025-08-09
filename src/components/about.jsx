8959e14d9347f17f0bfe785e51727f0de8ea03f3
Akshay Yelle <02b3akshay@gmail.com>
2025-08-10 00:04:59 +0530

    adding class components

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,15 @@
+import { User } from "./user"
+import UserClass from "./user-class"
+
 const About = () => {
   return (
     <div>
       <h1>About us</h1>
       <h2>This is about us page</h2>
+      <div className="flex flex-row gap m-4">
+        <User name="Akshay (functional component)" />
+        <UserClass name="Akshay (class component)" location="Bangalore (class)" />
+      </div>
     </div>
   )
 }
