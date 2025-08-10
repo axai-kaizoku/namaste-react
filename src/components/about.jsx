0dd32cbb4d5b1bd51ecb91c8a012d719cc21f832
Akshay Yelle <02b3akshay@gmail.com>
2025-08-10 16:41:06 +0530

    udpate class components state

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,17 +1,32 @@
+import React from "react"
 import { User } from "./user"
 import UserClass from "./user-class"
 
-const About = () => {
-  return (
-    <div>
-      <h1>About us</h1>
-      <h2>This is about us page</h2>
-      <div className="flex flex-row gap m-4">
-        <User name="Akshay (functional component)" />
-        <UserClass name="Akshay (class component)" location="Bangalore (class)" />
+class About extends React.Component {
+  constructor(props) {
+    super(props)
+
+    console.log("Parent Constructor")
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount")
+  }
+
+  render() {
+    console.log("Parent Render")
+
+    return (
+      <div>
+        <h1>About us</h1>
+        <h2>This is about us page</h2>
+        <div className="flex flex-row gap m-4">
+          <UserClass name="First" location="" />
+          <UserClass name="Second" location="" />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default About
--- src/components/user-class.jsx
+++ src/components/user-class.jsx
@@ -6,17 +6,43 @@ class UserClass extends React.Component {
 
     this.state = {
       count: 0,
-      count2: 2,
     }
+
+    console.log(this.props.name + "Constructor ")
+  }
+
+  async componentDidMount() {
+    console.log(this.props.name + "Component Did Mount")
+
+    // const res = await fetch("https://api.github.com/users/axai-kaizoku")
+
+    // const data = await res.json()
+
+    // console.log(data)
+  }
+
+  componentDidUpadate() {
+    console.log(this.props.name + "Component Did Update")
   }
 
   render() {
+    console.log(this.props.name + "Render")
     const { name, location } = this.props
-    const { count, count2 } = this.state
+    const { count } = this.state
+
     return (
       <div className="border flex flex-col gap w-fit p-4 rounded m-4">
-        <h1>Count: {count}</h1>
-        <h1>Count2: {count2}</h1>
+        <p>Count: {count}</p>
+        <button
+          className="btn"
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            })
+          }}
+        >
+          Increase Count
+        </button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <p>Contact: @axai-kaizoku</p>
